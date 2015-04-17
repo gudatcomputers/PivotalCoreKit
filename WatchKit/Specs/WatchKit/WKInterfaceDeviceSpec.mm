a5976a28f5a1e334122791fff74a3da1e3b4e03b
Jason McKee <mckeejm@gmail.com>
2015-04-16 21:40:58 -0400

    add support for WKInterfaceDevice

--- WatchKit/Specs/WatchKit/WKInterfaceDeviceSpec.mm
+++ WatchKit/Specs/WatchKit/WKInterfaceDeviceSpec.mm
@@ -1,16 +1,54 @@
-#import <Cedar/Cedar.h>
-#import "WKInterfaceDeviceSpec.h"
+#import "Cedar.h"
+#import "WKInterfaceDevice.h"
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
 
-SPEC_BEGIN(WKInterfaceDeviceSpecSpec)
+SPEC_BEGIN(WKInterfaceDeviceSpec)
 
-describe(@"WKInterfaceDeviceSpec", ^{
-    __block WKInterfaceDeviceSpec *<#object under test#>;
+describe(@"WKInterfaceDevice", ^{
+    __block WKInterfaceDevice *subject;
 
     beforeEach(^{
+        subject = [[WKInterfaceDevice alloc] init];
+    });
 
+    describe(@"public methods", ^{
+        __block NSString *name;
+
+        beforeEach(^{
+            name = @"image-name";
+        });
+
+        it(@"should record the invocation for addCachedImage:name:", ^{
+            UIImage *image = [[UIImage alloc] init];
+
+            [subject addCachedImage:image name:name];
+
+            subject should have_received(@selector(addCachedImage:name:))
+            .with(image, name);
+        });
+
+        it(@"should record the invocation for addCachedImageWithData:name:", ^{
+            NSData *imageData = [[NSData alloc] init];
+            [subject addCachedImageWithData:imageData name:name];
+
+            subject should have_received(@selector(addCachedImageWithData:name:))
+            .with(imageData, name);
+        });
+
+        it(@"should record the invocation removeCachedImageWithName:", ^{
+            [subject removeCachedImageWithName:name];
+
+            subject should have_received(@selector(removeCachedImageWithName:))
+            .with(name);
+        });
+
+        it(@"should record the invocation for removeAllCachedImages", ^{
+            [subject removeAllCachedImages];
+
+            subject should have_received(@selector(removeAllCachedImages));
+        });
     });
 });
 
